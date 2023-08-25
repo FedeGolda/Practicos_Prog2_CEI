ed7779b7aa2aac5cbfd20add77d54074e5fab795
FedeGolda <federico.goldaracena@hotmail.com>
2023-08-25 17:26:44 -0300

    Agregando

---
+++ Practico_ej2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Practico_ej2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado = 0;
+            Console.WriteLine("Ingresar el lado del cuadrado: ");
+            lado = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lado; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+
+            for (int i = 2; i < lado; i++)
+            {
+                Console.Write("*");
+                for (int j = 2; j < lado; j++)
+                {
+                    Console.Write(" ");
+                }
+            Console.WriteLine("*");
+            }
+
+            for (int i = 0; i < lado; i++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}
