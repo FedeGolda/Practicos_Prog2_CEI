65f8019cb45907e1d6af460a475e27bcafbd73f6
FedeGolda <federico.goldaracena@hotmail.com>
2023-08-25 19:14:42 -0300

    agregando practico ej 6 terminado

---
+++ Practico_ej6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practico_ej6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicitamos al usuario que ingrese un año
+            Console.Write("Introduce un año: ");
+            int anio = Int32.Parse(Console.ReadLine());
+
+            // Comprobamos si el año es bisiesto utilizando una única expresión booleana
+            bool esBisiesto = (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
+
+            // Mostramos el resultado basado en el valor de esBisiesto
+            if (esBisiesto)
+            {
+                Console.WriteLine($"\nEl año {anio} es bisiesto\n");
+            }
+            else
+            {
+                Console.WriteLine($"\nEl año {anio} no es bisiesto\n");
+            }
+        }
+    }
+}
