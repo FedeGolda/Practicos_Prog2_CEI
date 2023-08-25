3b52f3f953c537355701daf7523b6a52d510e112
FedeGolda <federico.goldaracena@hotmail.com>
2023-08-24 21:47:27 -0300

    Agregando Practico ejercicio 1

---
+++ Practico_ej1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+
+namespace Practico_ej1
+{
+    namespace Condicional_switch
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int dia = 0;
+                int mes = 0;
+                int anio = 0;
+
+                Console.WriteLine("Ingrese Dia: ");
+                dia = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nIngrese Mes: ");
+                mes = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nIngrese Año: ");
+                anio = Int32.Parse(Console.ReadLine());
+
+                Console.Clear(); // Limpia la pantalla en la consola
+
+                if ((dia >= 1 && dia <= 31) && (mes >= 1 && mes <= 12) && anio > 0)
+                {
+                    switch (mes)
+                    {
+                        case 1:
+                            Console.WriteLine($"{dia} de Enero de {anio}");
+                            break;
+
+                        case 2:
+                            Console.WriteLine($"{dia} de Febrero de {anio}");
+                            break;
+
+                        case 3:
+                            Console.WriteLine($"{dia} de Marzo de {anio}");
+                            break;
+
+                        case 4:
+                            Console.WriteLine($"{dia} de Abril de {anio}");
+                            break;
+
+                        case 5:
+                            Console.WriteLine($"{dia} de Mayo de {anio}");
+                            break;
+
+                        case 6:
+                            Console.WriteLine($"{dia} de Junio de {anio}");
+                            break;
+
+                        case 7:
+                            Console.WriteLine($"{dia} de Julio de {anio}");
+                            break;
+
+                        case 8:
+                            Console.WriteLine($"{dia} de Agosto de {anio}");
+                            break;
+
+                        case 9:
+                            Console.WriteLine($"{dia} de Setiembre de {anio}");
+                            break;
+
+                        case 10:
+                            Console.WriteLine($"{dia} de Octubre de {anio}");
+                            break;
+
+                        case 11:
+                            Console.WriteLine($"{dia} de Noviembre de {anio}");
+                            break;
+
+                        case 12:
+                            Console.WriteLine($"{dia} de Diciembre de {anio}");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: LA FECHA ESTA MAL...\t");
+                }
+            }
+        }
+    }
+}
+          
