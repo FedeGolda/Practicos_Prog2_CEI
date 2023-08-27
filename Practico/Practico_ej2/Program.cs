aa63b126d3edaf8d9df752ba026b1c93b7d2fcdd
FedeGolda <federico.goldaracena@hotmail.com>
2023-08-27 17:49:44 -0300

    agregando comentarios

--- Practico/Practico_ej2/Program.cs
+++ Practico/Practico_ej2/Program.cs
@@ -5,32 +5,38 @@
         static void Main(string[] args)
         {
             int lado = 0;
-            Console.WriteLine("Ingresar el lado del cuadrado: ");
+            Console.Write("Ingresar el lado del cuadrado: ");
             lado = int.Parse(Console.ReadLine());
 
+            // Dibujar la primera fila de asteriscos
             for (int i = 0; i < lado; i++)
             {
                 Console.Write("*");
             }
-            Console.WriteLine("");
+            Console.WriteLine(""); // Cambiar de línea después de dibujar la primera fila
 
+            // Dibujar las filas intermedias
             for (int i = 2; i < lado; i++)
             {
-                Console.Write("*");
+                Console.Write("*"); // Primer asterisco de cada fila
+
+                // Rellenar con espacios en las columnas intermedias
                 for (int j = 2; j < lado; j++)
                 {
                     Console.Write(" ");
                 }
-            Console.WriteLine("*");
+
+                Console.WriteLine("*"); // Último asterisco de cada fila
             }
 
+            // Dibujar la última fila de asteriscos
             for (int i = 0; i < lado; i++)
             {
                 Console.Write("*");
             }
 
-            Console.WriteLine("");
-            Console.ReadLine();
+            Console.WriteLine(""); // Cambiar de línea después de dibujar la última fila
+            Console.ReadLine(); // Esperar a que el usuario presione Enter antes de salir
         }
     }
 }
