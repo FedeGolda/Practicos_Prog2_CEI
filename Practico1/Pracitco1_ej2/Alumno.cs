0cfbd3aea4b0438628dbb8db12d8dad0df070aae
FedeGolda <federico.goldaracena@hotmail.com>
2023-08-29 19:47:20 -0300

    terminado Practico1_ej2

---
+++ Practico1/Pracitco1_ej2/Alumno.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pracitco1_ej2
+{
+    internal class Alumno
+    {
+        private string? nombre;
+        private int edad;
+/*
+        public Alumno()
+        {
+            nombre = "Fede";
+            edad = 30;
+        }
+*/
+        public Alumno(string infoNombre, int infoEdad)
+        {
+            nombre = infoNombre;
+            edad = infoEdad;
+        }
+
+
+        public string imprimirNombre()
+        {
+            return "* Nombre: " + nombre;
+        }
+
+        public string imprimirNombreEdad()
+        {
+            return "* Nombre y edad: " + nombre + ", " + edad;
+        }
+
+        public void setNombre(string infoNombre)
+        {
+            this.nombre = infoNombre;
+        }
+
+        public void setEdad(int infoEdad)
+        { 
+            this.edad = infoEdad;
+        }
+
+    }
+}
---
+++ Practico1/Pracitco1_ej2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pracitco1_ej2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno("Carlos", 28);
+            Alumno alumno2 = new Alumno("Juan", 25);
+
+            Console.WriteLine(alumno1.imprimirNombre());
+            Console.WriteLine(alumno1.imprimirNombreEdad());
+            Console.WriteLine(alumno2.imprimirNombreEdad());
+
+            // Modificando valores con los métodos Set
+            alumno1.setNombre("Andrea");
+            alumno1.setEdad(22);
+
+            Console.WriteLine(alumno1.imprimirNombre());
+            Console.WriteLine(alumno1.imprimirNombreEdad());
+
+        }
+    }
+}
