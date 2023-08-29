f7b494408ac7cb182809fd0fc9f309529ec1504d
FedeGolda <federico.goldaracena@hotmail.com>
2023-08-29 17:21:37 -0300

    Creando carpeta Practico1 y Practico1_ej1

---
+++ Practico1/Practico1_ej1/Auto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico1_ej1
+{
+    internal class Auto
+    {
+        // Declaracion de variables (propiedades) de la clase Auto. Solo puedo acceder desde la clase Auto
+        private string tapiceria;
+        private string climatizador;
+
+        public Auto() // Constructor del estado inicial. No recibe parametros, solo inicializa el objeto que lo llame
+        {
+            tapiceria = "tela";
+            climatizador = "compresor";
+        }
+
+        public Auto(string infoTapiceria, string tipoClimatizador) // Sobrecarga de constructores
+        {
+            tapiceria = infoTapiceria;
+            climatizador = tipoClimatizador;
+        }
+
+        // ------------------------------ Getters y Setters ---------------------
+
+        public string GetTapiceria()
+        {
+            return "Tapiceria:" + tapiceria;
+        }
+        public string GetTipoClimatizador()
+        {
+            return "Climatizador:" + climatizador;
+        }
+        public void SetTapiceria(string infoTapiceria)
+        {
+            this.tapiceria = infoTapiceria;
+        }
+
+        public void SetTipoClimatizador(string tipoClimatizador)
+        {
+            this.climatizador = tipoClimatizador;
+        }
+
+    }
+}
---
+++ Practico1/Practico1_ej1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practico1_ej1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear un objeto Auto usando el constructor sin parámetros
+            Auto auto1 = new Auto();
+            Console.WriteLine(auto1.GetTapiceria());
+            Console.WriteLine(auto1.GetTipoClimatizador());
+
+            // Crear otro objeto Auto usando el constructor sobrecargado
+            Auto auto2 = new Auto("Cuero", "Automático");
+            Console.WriteLine(auto2.GetTapiceria());
+            Console.WriteLine(auto2.GetTipoClimatizador());
+
+            // Cambiar la tapicería y el tipo de climatizador del segundo auto
+            auto2.SetTapiceria("Tela");
+            auto2.SetTipoClimatizador("Manual");
+            Console.WriteLine(auto2.GetTapiceria());
+            Console.WriteLine(auto2.GetTipoClimatizador());
+
+            Console.ReadLine();
+        }
+    }
+}
