d6e36cc1036aa66511b576c7bb285246d27ea64d
FedeGolda <federico.goldaracena@hotmail.com>
2023-08-31 19:51:22 -0300

    Archivo creado Practico1_ej3

---
+++ Practico1/Practico1_ej3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practico1_ej3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Practico1/Practico1_ej3/Vector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico1_ej3
+{
+    internal class Vector
+    {
+    }
+}
