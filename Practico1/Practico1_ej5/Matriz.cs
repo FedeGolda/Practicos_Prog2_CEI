6ecc2934a588cd079137c047923929450c0f4023
FedeGolda <federico.goldaracena@hotmail.com>
2023-08-31 23:30:03 -0300

    creando clase Matriz en Practico1_ej5

---
+++ Practico1/Practico1_ej5/Matriz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico1_ej5
+{
+    internal class Matriz
+    {
+        private int[,] matriz1;
+        private int[,] matriz2;
+
+        public Matriz(int x, int y)
+        {
+            matriz1 = new int[x, y];
+            matriz2 = new int[x, y];
+        }
+
+        public int GetMatriz1(int x, int y)
+        {
+            return matriz1[x, y];
+        }
+
+        public int GetMatriz2(int x, int y)
+        {
+            return matriz2[x, y];
+        }
+
+        public void SetMatriz1Value(int x, int y, int value)
+        {
+            matriz1[x, y] = value;
+        }
+
+        public void SetMatriz2Value(int x, int y, int value)
+        {
+            matriz2[x, y] = value;
+        }
+
+    }
+}
