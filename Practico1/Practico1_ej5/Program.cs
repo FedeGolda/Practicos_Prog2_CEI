765e2a8489401a52497a59f5cc703f375f60dfca
FedeGolda <federico.goldaracena@hotmail.com>
2023-08-31 23:58:39 -0300

    correciones

--- Practico1/Practico1_ej5/Program.cs
+++ Practico1/Practico1_ej5/Program.cs
@@ -6,16 +6,17 @@ namespace Practico1_ej5
     {
         static void Main(string[] args)
         {
-            int rows = 3;
-            int cols = 3;
+            int x = 3;
+            int y = 3;
 
-            Matriz matriz1 = new Matriz(rows, cols);
-            Matriz matriz2 = new Matriz(rows, cols);
+            Matriz matriz1 = new Matriz(x, y);
+            Matriz matriz2 = new Matriz(x, y);
+
 
             // Configurar valores en las matrices matriz1 y matriz2
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < x; i++)
             {
-                for (int j = 0; j < cols; j++)
+                for (int j = 0; j < y; j++)
                 {
                     matriz1.SetMatriz1Value(i, j, i + j);  // Ejemplo: Configurando valores en matriz1
                     matriz2.SetMatriz2Value(i, j, i - j);  // Ejemplo: Configurando valores en matriz2
@@ -25,9 +26,9 @@ namespace Practico1_ej5
             // Calcular y mostrar la suma de las matrices
             int[,] suma = matriz1.SumarMatrices();
             Console.WriteLine("Suma de las matrices:");
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < x; i++)
             {
-                for (int j = 0; j < cols; j++)
+                for (int j = 0; j < y; j++)
                 {
                     Console.Write(suma[i, j] + "\t");
                 }
