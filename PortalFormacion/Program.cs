47ae090ed6583b1ce9e7e49e99fd277c41619e3c
FedeGolda <federico.goldaracena@hotmail.com>
2023-09-08 12:33:03 -0300

    Creado PortalFormacion

---
+++ PortalFormacion/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PortalFormacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ PortalFormacion/Usuario.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalFormacion
+{
+    internal class Usuario
+    {
+        protected int id;
+        protected string nombre;
+        protected string apellido;
+
+        public Usuario(int id, string nombre, string apellido)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        public int getId() 
+        {
+            return id;
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public string getApellido()
+        {
+            return apellido;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void setApellido(string apellido)
+        {
+            this.apellido = apellido;
+        }
+
+
+        public void Login(string nombre, string apellido)
+        {
+            
+        }
+
+    }
+}
