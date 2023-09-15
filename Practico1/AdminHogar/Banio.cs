1662935ace07376525cf00190f6ff98c81a2bb23
FedeGolda <federico.goldaracena@hotmail.com>
2023-09-14 21:00:54 -0300

    Se crean la clase Banio y Cocina

---
+++ Practico1/AdminHogar/Banio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminHogar
+{
+    internal class Banio : Hogar
+    {
+        private int cantidad;
+
+        public Banio(int cantidad, string nombreBanio) : base(nombreBanio)
+        {
+            this.cantidad = cantidad;
+        }
+
+        public int GetCantidad()
+        {
+            return cantidad;
+        }
+    }
+}
