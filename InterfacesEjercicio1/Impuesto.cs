f5cf723bdfa185de1f4a8f4ed2d30be6b6bab857
FedeGolda <federico.goldaracena@hotmail.com>
2023-10-12 20:18:24 -0300

    Agregando clases

---
+++ InterfacesEjercicio1/Impuesto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesEjercicio1
+{
+    internal class Impuesto : Iimprimible
+    {
+        private int importe;
+        public Impuesto(int importe)
+        {
+            this.importe = importe;
+        }
+
+        public int getImporte() { return importe; }
+        public void setImporte(int importe) { this.importe = importe; }
+
+        //Metodos obligatorios
+        public void Imprimir()
+        {
+            Console.WriteLine($"Impresion de Importe: {importe}");
+        }
+    }
+}
---
+++ InterfacesEjercicio1/ReciboSueldo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesEjercicio1
+{
+    internal class ReciboSueldo : Iimprimible
+    {
+        private int legajo;
+        private int total;
+
+        public ReciboSueldo(int legajo, int total)
+        {
+            this.legajo = legajo;
+            this.total = total;
+        }
+
+        public int getLegajo() { return legajo; }
+        public void setLegajo(int legajo) { this.legajo = legajo; }
+
+        public int getTotal() { return total; }
+        public void setTotal(int total) { this.total = total; }
+
+        //Metodos obligatorios
+        public void Imprimir()
+        {
+            Console.WriteLine($"Impresion de Legajo {legajo} de total: ${total}");
+        }
+    }
+}
