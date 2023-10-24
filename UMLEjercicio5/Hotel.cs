136b6c57262ac18b96406f6fab1f6a67710a1946
FedeGolda <federico.goldaracena@hotmail.com>
2023-10-23 22:07:29 -0300

    Creado UMLEjercicio 5 y creado clase Hotel

---
+++ UMLEjercicio5/Hotel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMLEjercicio5
+{
+    internal class Hotel
+    {
+        private string nombre;
+        private string direccion;
+
+        public Hotel()
+        {
+
+        }
+
+        public Hotel(string nombre, string direccion)
+        {
+            this.nombre = nombre;
+            this.direccion = direccion;
+        }
+
+        public string getNombre() => this.nombre;
+        public void setNombre(string nombre) => this.nombre = nombre;
+        public string getDireccion() => this.direccion;
+        public void setDireccion(string direccion) => this.direccion = direccion;
+
+    }
+}
