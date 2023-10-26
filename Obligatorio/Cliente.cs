4afa431caa284d8440c1f16befe2b7d2bb337fec
FedeGolda <federico.goldaracena@hotmail.com>
2023-10-26 13:23:37 -0300

    Creado Obligatorio y sus clases

---
+++ Obligatorio/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio
+{
+    internal class Cliente
+    {
+        private string nombre;
+        private string cedula;
+
+        public Cliente(string nombre, string cedula)
+        {
+            this.nombre = nombre;
+            this.cedula = cedula;
+        }
+
+        public string getNombre() => this.nombre;
+        public string getCedula() => this.cedula;
+        public void setNombre(string nombre) => this.nombre = nombre;
+        public void setCedula(string cedula) => this.cedula = cedula;
+    }
+}
---
+++ Obligatorio/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio
+{
+    internal class Usuario
+    {
+        private string nombre;
+        private string cedula;
+        private List<string> permisos;
+        
+        public Usuario(string nombre, string cedula, List<string> permisos)
+        {
+            this.nombre = nombre;
+            this.cedula = cedula;
+            this.permisos = permisos;
+        }
+
+        public string getNombre() => this.nombre;
+        public void setNombre(string nombre) => this.nombre = nombre;
+        public string getCedula() => this.cedula;
+        public void setCedula(string cedula) => this.cedula = cedula;
+        public List<string> getPermisos() => this.permisos;
+        public void setPermisos(List<string> permisos) => this.permisos = permisos;
+
+    }
+}
