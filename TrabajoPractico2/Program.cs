022d9e6f82b0863a4f958c235f9b4961979cb70a
FedeGolda <federico.goldaracena@hotmail.com>
2023-11-01 11:56:05 -0300

    Agregando metodos e instancias

--- TrabajoPractico2/Program.cs
+++ TrabajoPractico2/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Vehiculo> colVehiculos = new List<Vehiculo>();
+            List<Alquiler> colAlquileres = new List<Alquiler>();
+            List<Detalle> colDetalles = new List<Detalle>();
+
+            // Crear una instancia de la clase Sucursal
+            Sucursal sucursal = new Sucursal(1, "Dirección de la Sucursal", colVehiculos, colAlquileres);
+
+            // Crear instancias de Vehiculo
+            Vehiculo vehiculo1 = new Vehiculo(1, "ABC123", "Toyota", "Rojo", 50, "Disponible", 50.0, 15);
+            Vehiculo vehiculo2 = new Vehiculo(2, "DEF456", "Ford", "Azul", 45, "Disponible", 45.0, 18);
+            Vehiculo vehiculo3 = new Vehiculo(3, "GHI789", "Chevrolet", "Verde", 55, "Disponible", 55.0, 14);
+            Vehiculo vehiculo4 = new Vehiculo(4, "JKL012", "Honda", "Amarillo", 48, "Disponible", 48.0, 17);
+            Vehiculo vehiculo5 = new Vehiculo(5, "MNO345", "Nissan", "Negro", 52, "Disponible", 52.0, 16);
+
+            // Crear instancias de Alquiler
+            Alquiler alquiler1 = new Alquiler(1, 200.0, "12345678", "123456789", "Cliente1", "Apellido1");
+            Alquiler alquiler2 = new Alquiler(2, 300.0, "23456789", "234567890", "Cliente2", "Apellido2");
+            Alquiler alquiler3 = new Alquiler(3, 150.0, "34567890", "345678901", "Cliente3", "Apellido3");
+
+            // Crear instancias de Detalle y asociarlas con Alquiler
+            Detalle detalle1 = new Detalle(vehiculo1, DateTime.Now, 3);
+            Detalle detalle2 = new Detalle(vehiculo2, DateTime.Now, 5);
+            Detalle detalle3 = new Detalle(vehiculo3, DateTime.Now, 2);
+
+            // Registrar vehículos en la sucursal
+            sucursal.RegistrarVehiculo(vehiculo1);
+            sucursal.RegistrarVehiculo(vehiculo2);
+            sucursal.RegistrarVehiculo(vehiculo3);
+            sucursal.RegistrarVehiculo(vehiculo4);
+            sucursal.RegistrarVehiculo(vehiculo5);
+
+            // Registrar alquileres en la sucursal
+            sucursal.RegistrarAlquiler(alquiler1);
+            sucursal.RegistrarAlquiler(alquiler2);
+            sucursal.RegistrarAlquiler(alquiler3);
+
+            alquiler1.AgregarDetalle(detalle1);
+            alquiler2.AgregarDetalle(detalle2);
+            alquiler3.AgregarDetalle(detalle3);
+
+            // Mostrar vehículos y alquileres
+            sucursal.ListarVehiculos();
+            sucursal.ListarAlquileres();
         }
     }
 }
--- TrabajoPractico2/Sucursal.cs
+++ TrabajoPractico2/Sucursal.cs
@@ -11,7 +11,7 @@ namespace TrabajoPractico2
         private int numero;
         private string direccion;
         private List<Vehiculo> colVehiculos; // Relacion de Asociacion. Tengo una lista de Vehiculos 1 ---> 1..*
-        private List<Alquiler> colAlquileres; // Relacion de Asociacion. Tengo una lista de Vehiculos 1 ---> 1..*
+        private List<Alquiler> colAlquileres; // Relacion de Asociacion. Tengo una lista de Alquiler 1 ---> 1..*
         public Sucursal(int numero, string direccion, List<Vehiculo> colVehiculos, List<Alquiler> colAlquileres)
         {
             this.numero = numero;
@@ -28,5 +28,42 @@ namespace TrabajoPractico2
         public void setColVehiculos(List<Vehiculo> colVehiculos) => this.colVehiculos = colVehiculos;
         public List<Alquiler> GetAlquileres() => this.colAlquileres;
         public void setColAlquileres(List<Alquiler> colAlquileres) => this.colAlquileres = colAlquileres;
+
+        public void RegistrarVehiculo(Vehiculo vehiculo)
+        {
+            colVehiculos.Add(vehiculo);
+            //Console.WriteLine("Vehículo registrado en la sucursal exitosamente.");
+        }
+
+        public void RegistrarAlquiler(Alquiler alquiler)
+        {
+            colAlquileres.Add(alquiler);
+            //Console.WriteLine("Alquiler registrado en la sucursal exitosamente.");
+        }
+
+        public void ListarVehiculos()
+        {
+            Console.WriteLine("Vehículos en la sucursal:");
+            foreach (var vehiculo in colVehiculos)
+            {
+                Console.WriteLine($"Número: {vehiculo.getNumero()}, Matrícula: {vehiculo.getMatricula()}, Marca: {vehiculo.getMarca()}");
+            }
+        }
+
+        public void ListarAlquileres()
+        {
+            Console.WriteLine("\nAlquileres en la sucursal:");
+            foreach (var alquiler in colAlquileres)
+            {
+                Console.WriteLine($"Número de alquiler: {alquiler.getNumero()}, Precio total: {alquiler.getPrecioTotal()}, Cliente: {alquiler.getNombre()} {alquiler.getApellido()}");
+                Console.WriteLine("Vehículos en el alquiler:");
+                foreach (var detalle in alquiler.getColDetalles())
+                {
+                    var vehiculo = detalle.getVehiculo();
+                    Console.WriteLine($"   Número: {vehiculo.getNumero()}, Matrícula: {vehiculo.getMatricula()}, Marca: {vehiculo.getMarca()}");
+                }
+            }
+        }
     }
 }
+
